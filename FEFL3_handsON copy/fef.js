9d7ee2e3e1d6540aed7c2767b80d684325ddbfa0
Vince Oblak II <voblak2@gmail.com>
2019-10-11 18:38:19 -0500

    Add files via upload

---
+++ FEFL3_handsON copy/fef.js
@@ -0,0 +1,23 @@
+let highSchoolGrade = 11;
+let yourGrade;
+
+
+switch (highSchoolGrade){
+case 9:
+  yourGrade = 'Freshman';
+  break;
+case 10: 
+  yourGrade = 'Sophmore';
+  break;
+case 11:
+  yourGrade = 'Junior';
+break;
+case 12:  
+  yourGrade = 'Senior';
+break;
+  default:
+  yourGrade = 'Invalid';
+}
+console.log(highSchoolGrade);
+
+  let firstName = 'John' ? document.getElementById("demo").innerHTML = "Hello John!" : document.getElementById("demo").innerHTML = "Hello Human!";
